0bb44ea27a55eab169047b627de9c968de7026a0
dizsun <dizsun@icloud.com>
2016-11-13 10:29:17 +0800

    Initial commit

---
+++ app/src/main/java/com/dizsun/renminers/ReminderSimpleCursorAdapter.java
@@ -0,0 +1,47 @@
+package com.dizsun.renminers;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.util.ArrayMap;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by sundiz on 16/11/12.
+ */
+
+public class ReminderSimpleCursorAdapter extends SimpleCursorAdapter {
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return super.newView(context, cursor, parent);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        super.bindView(view, context, cursor);
+        ViewHolder holder = (ViewHolder) view.getTag();
+        if (holder == null) {
+            holder = new ViewHolder();
+            holder.colImp = cursor.getColumnIndexOrThrow(RemindersDbAdapter.COL_IMPORTANT);
+            holder.listTab = view.findViewById(R.id.row_tab);
+            view.setTag(holder);
+        }
+        if (cursor.getInt(holder.colImp) > 0) {
+            holder.listTab.setBackgroundColor(context.getColor(R.color.colorOrange));
+        } else {
+            holder.listTab.setBackgroundColor(context.getColor(R.color.colorGreen));
+        }
+    }
+
+    public ReminderSimpleCursorAdapter(Context context, int layout, Cursor c, String[] from, int[] to, int flags) {
+        super(context, layout, c, from, to, flags);
+
+    }
+
+    static class ViewHolder {
+        int colImp;
+        View listTab;
+    }
+}
